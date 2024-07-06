b3939f29a4be237e16f3178a29f5dc8c9344003a
Shola Abhulimhen <sholaabhus@gmail.com>
2024-07-06 20:04:43 +0000

    update App

--- exercises/algorithms/sort/src/main/java/com/cbfacademy/sort/App.java
+++ exercises/algorithms/sort/src/main/java/com/cbfacademy/sort/App.java
@@ -9,15 +9,10 @@ VisualQuickSort sorter = new VisualQuickSort();
 int[]arr =new int[] {55,18,-9,3,107,24,66,0};
 
 System.out.println("print original number" + Arrays.toString(arr));
-
 sorter.display(arr, 0, arr.length - 1, -1, arr.length - 1, -1);
 //ArraySorter sorter = new VisualQuickSort();
 //sort.QuickSort(arr, 0, 7);
 sorter.sort(arr,0,7);
-
-
-
-
 System.out.println("sorted Array" + Arrays.toString(arr));
 sorter.display(arr, 0, arr.length - 1, -1, -1, -1);
 
